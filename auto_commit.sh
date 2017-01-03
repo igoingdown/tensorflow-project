29bfb654f8762d903a77211703a16ebbf110d0f7
igoingdown <fycjmingxing@126.com>
2017-01-03 16:13:46 +0800

    2017.1.3

--- auto_commit.sh
+++ auto_commit.sh
@@ -2,5 +2,7 @@
 
 git status >> ../git.out 2>&1
 git add . >> ../git.out 2>&1
-git commit -m "2017.1.3" >> ../git.out 2>&1
+s_1=`date +%F`
+s_2=`date +%T`
+git commit -m "time is: $s_1 $s_2" >> ../git.out 2>&1
 git push -u origin master >> ../git.out 2>&1
--- shell_test.sh
+++ shell_test.sh
@@ -5,3 +5,9 @@ do
     cat $file
 done
 
+s_1=`date +%F`
+s_2=`date +%T`
+echo $s_1
+echo $s_2
+echo "time is: $s_1 $s_2"
+
