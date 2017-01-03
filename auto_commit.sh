914553e536ff1afb1a93e5a30fa2ee22022fe2c0
igoingdown <fycjmingxing@126.com>
2017-01-03 16:30:28 +0800

    commit time: 2017-01-03 16:30:28

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-git status >> ../git.out 2>&1
-git add . >> ../git.out 2>&1
+git status >> /dev/null 2>&1
+git add . >> /dev/null 2>&1
 s_1=`date +%F`
 s_2=`date +%T`
-git commit -m "time is: $s_1 $s_2" >> ../git.out 2>&1
-git push -u origin master >> ../git.out 2>&1
+git commit -m "commit time: $s_1 $s_2" >> /dev/null 2>&1
+git push -u origin master >> /dev/null 2>&1
