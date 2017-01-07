f3a5f50f47816963ebf6ab49af2c612089a26fee
igoingdown <fycjmingxing@126.com>
2017-01-07 12:13:59 +0800

    commit time: 2017-01-07 12:13:59

--- git_create_repository_and_link_to_github.sh
+++ git_create_repository_and_link_to_github.sh
@@ -2,7 +2,9 @@
 
 # create a local git repository and link it to the github repository.
 # to link a local git repository to the github, we need a github repository!
-# give the repository as arg!
+
+# give the online github repository whole path as the first arg!
+# like "git@github.com:igoingdown/tensorflow-project.git"
 
 git init
 git config --global user.name "igoingdown"
