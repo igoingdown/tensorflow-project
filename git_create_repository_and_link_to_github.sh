9abf238ba098a6a64688e214f0bcea2a7b37fdd9
igoingdown <fycjmingxing@126.com>
2017-01-07 12:09:08 +0800

    commit time: 2017-01-07 12:09:08

---
+++ git_create_repository_and_link_to_github.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# create a local git repository and link it to the github repository.
+# to link a local git repository to the github, we need a github repository!
+# give the repository as arg!
+
+git init
+git config --global user.name "igoingdown"
+git config --global user.email "fycjmingxing@126.com"
+if [ $1 == "" ]; then
+    echo -e "\n\nwe need a github repository as the first arg!\n\n"
+    exit 1
+else
+    git remote add origin "$1"
+fi
--- shell_test.sh
+++ shell_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in `ls `
+for file in `ls -R`
 do
     cat $file
 done
@@ -11,3 +11,9 @@ echo $s_1
 echo $s_2
 echo "time is: $s_1 $s_2"
 
+if [ $1 == "start" ]; then
+    echo "$1"
+else
+    echo -e "\nwe need a github repository as first arg!"
+    exit 1
+fi
