b0413edc14a64db19f5485a9f62ed91eb3b91122
igoingdown <fycjmingxing@126.com>
2017-01-10 12:07:42 +0800

    commit time: 2017-01-10 12:07:42

--- CommitModificationOnGIthubRepositoriesCasually.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-tensorflow_project_path="/Users/zhaomingxing/PycharmProjects/test_tensor"
-
-cd $tensorflow_project_path
-bash auto_commit.sh
-
-# bash /Users/zhaomingxing/IdeaProjects/hello_world/src/package_1/auto_commit.sh
-
