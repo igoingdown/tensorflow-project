e0bfb29d79de1e5355a1e7a9532f8972c57f088d
igoingdown <fycjmingxing@126.com>
2017-01-10 14:54:39 +0800

    commit time: 2017-01-10 14:54:39

--- auto_commit.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-git status >> /dev/null 2>&1
-git add . >> /dev/null 2>&1
-s_1=`date +%F`
-s_2=`date +%T`
-remoteOrigin=`git remote`
-git commit -m "commit time: $s_1 $s_2" >> /dev/null 2>&1
-git push -u "$remoteOrigin" master >> /dev/null 2>&1
