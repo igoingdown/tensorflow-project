659e6609fd85413dfa86fb937e2bb11c442db99a
igoingdown <fycjmingxing@126.com>
2017-02-11 11:29:16 +0800

    commit time: 2017-02-11 11:29:16

---
+++ test_nn_scratch.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""
+===============================================================================
+author: 赵明星
+desc:   手写神经网络，不使用tensorflow类似的高级架构。
+===============================================================================
+"""
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import numpy as np
+from sklearn import datasets, linear_model
+import matplotlib.pyplot as plt
+
+
+class Config:
+    nn_input_dim = 2
+    nn_output_dim = 2
+    epsilon = 0.01
+    reg_lambda = 0.01
+
+
+def generate_data():
+    np.random.seed(0)
+    X, y = datasets.make_moons(200, noise=0.20)
+    return X, y
+
+
+def visualize(X, y, model):
+    pass
+
+
+if __name__ == "__main__":
+    X, y = generate_data()
+    print len(X)
+    print X, "\n\n\n"
+    print y
+    plt.scatter(X[:, 0], X[:, 1], s=40, c=y, cmap=plt.cm.Spectral)
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
